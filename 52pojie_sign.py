2566e86c05200cb48dc73911356394d1acdfa7ff
lqkxs3608 <67536525+lqkxs3608@users.noreply.github.com>
2021-03-16 18:36:39 +0800

    Update 52pojie_sign.py

--- 52pojie_sign.py
+++ 52pojie_sign.py
@@ -56,17 +56,16 @@ class WxNotify:
 
 cookie=""
 if not cookie:
-    cookie = input("cookie")
+    cookie = input("输入cookie")
 QYWX_AM = ""
 if not QYWX_AM:
-    QYWX_AM = input("QYWX_AM")
+    QYWX_AM = input("输入QYWX_AM")
 url = 'https://www.52pojie.cn/home.php?mod=task&do=draw&id=2'
 url1 = 'https://www.52pojie.cn/home.php?mod=task&do=apply&id=2'
 headers = {'cookie':cookie,
            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4265.0 Safari/537.36 Edg/87.0.644.4'}
 requests.get(url1, headers=headers)
-req = requests.get(url, headers=headers).text
-    
+req = requests.get(url, headers=headers).text    
 doc = pq(req)
 msg = doc('.vwmy a').text() + '\t' + doc('#messagetext p').text()
 print(msg)
