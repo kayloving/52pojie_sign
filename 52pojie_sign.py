0f79e3f96fc2e954d0380d37c1b796abf29df0e1
lqkxs3608 <67536525+lqkxs3608@users.noreply.github.com>
2021-03-16 18:39:17 +0800

    Update 52pojie_sign.py

--- 52pojie_sign.py
+++ 52pojie_sign.py
@@ -70,6 +70,7 @@ doc = pq(req)
 msg = doc('.vwmy a').text() + '\t' + doc('#messagetext p').text()
 print(msg)
 print(QYWX_AM)
+print(cookie)
 # if not cookie:
 #     print('cookie为空')
 # corpid,corpsecret,agentid,media_id = QYWX_AM
