c7cbfd3f556a6e10b6ddfa6cec5f5c34bbd4e409
lqkxs3608 <67536525+lqkxs3608@users.noreply.github.com>
2021-03-17 16:12:51 +0800

    Update 52pojie_sign.py

--- 52pojie_sign.py
+++ 52pojie_sign.py
@@ -6,6 +6,7 @@ import os
 
 
 cookie = os.getenv("cookie")
+print cookie
 url = 'https://www.52pojie.cn/home.php?mod=task&do=draw&id=2'
 url1 = 'https://www.52pojie.cn/home.php?mod=task&do=apply&id=2'
 headers = {'cookie': cookie,
@@ -21,5 +22,6 @@ QYWX_CORPID = os.getenv("QYWX_CORPID")
 QYWX_CORPSECRET = os.getenv("QYWX_CORPSECRET")
 QYWX_AGENTID = os.getenv("QYWX_AGENTID")
 QYWX_MEDIA_ID = os.getenv("QYWX_MEDIA_ID")
+print QYWX_MEDIA_ID
 wn = WxNotify(corpid=QYWX_CORPID, corpsecret=QYWX_CORPSECRET, agentid=QYWX_AGENTID, media_id=QYWX_MEDIA_ID)
 wn.send('52破解签到信息', msg)
