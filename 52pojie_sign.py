9d908a4f152531bf534ea7b6ce478088fd2175d6
lqkxs3608 <67536525+lqkxs3608@users.noreply.github.com>
2021-03-17 14:44:30 +0800

    Update 52pojie_sign.py

--- 52pojie_sign.py
+++ 52pojie_sign.py
@@ -1,82 +1,34 @@
 # -- coding: utf-8 --
 import requests
 from pyquery import PyQuery as pq
-import json
+from .QYWX_Notify import WxNotify
 
 
-class WxNotify:
-    def __init__(self, corpid, corpsecret, agentid,media_id):
-        self.corpid = corpid
-        self.corpsecret = corpsecret
-        self.agentid = agentid
-        self.media_id = media_id
-        self.access_token = self.__get_access_token(corpid, corpsecret)
-
-    def __get_access_token(self, corpid, corpsecret):
-        url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken'
-        params = {
-            'corpid': corpid,
-            'corpsecret': corpsecret
-        }
-        resp = requests.get(url, params=params)
-        resp.raise_for_status()
-        resp_json = resp.json()
-        if 'access_token' in resp_json.keys():
-            return resp_json['access_token']
-        else:
-            raise Exception('Please check if corpid and corpsecret are correct \n' + resp.text)
-
-    def send(self, title, text):
-        url = 'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=' + self.access_token
-        data = {
-            "touser": "@all",
-            "msgtype": "mpnews",
-            "agentid": self.agentid,
-            "mpnews": {
-                "articles": [
-                    {
-                        "title": title,
-                        "thumb_media_id":self.media_id,
-                        "author": "",
-                        "content_source_url": "",
-                        "content": text,
-                        "digest": text
-                    }
-                ]
-            },
-            "safe": 0,
-            "enable_id_trans": 0,
-            "enable_duplicate_check": 0,
-            "duplicate_check_interval": 1800
-        }
-        resp = requests.post(url, data=json.dumps(data))
-        resp.raise_for_status()
-        return resp.json()
-    
-
-cookie=""
+cookie = ""
 if not cookie:
-    cookie = input("输入cookie")
-QYWX_AM = ""
-if not QYWX_AM:
-    QYWX_AM = input("输入QYWX_AM")
+    cookie = input("cookie")
+QYWX_CORPID = ""
+if not QYWX_CORPID:
+    QYWX_CORPID = input("QYWX_CORPID")
+QYWX_CORPSECRET = ""
+if not QYWX_CORPSECRET:
+    QYWX_CORPSECRET = input("QYWX_CORPSECRET")
+QYWX_AGENTID = ""
+if not QYWX_AGENTID:
+    QYWX_AGENTID = input("QYWX_AGENTID")
+QYWX_MEDIA_ID = ""
+if not QYWX_MEDIA_ID:
+    QYWX_MEDIA_ID = input("QYWX_MEDIA_ID")
 url = 'https://www.52pojie.cn/home.php?mod=task&do=draw&id=2'
 url1 = 'https://www.52pojie.cn/home.php?mod=task&do=apply&id=2'
-headers = {'cookie':cookie,
+headers = {'cookie': cookie,
            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4265.0 Safari/537.36 Edg/87.0.644.4'}
 requests.get(url1, headers=headers)
-req = requests.get(url, headers=headers).text    
+req = requests.get(url, headers=headers).text
 doc = pq(req)
 msg = doc('.vwmy a').text() + '\t' + doc('#messagetext p').text()
 print(msg)
-QYWX_A = QYWX_AM.split(';')
-corpid = QYWX_A[0]
-corpsecret =QYWX_A[1]
-agentid =QYWX_A[2]
-media_id =QYWX_A[3]
 if not cookie:
     print('cookie为空')
-wn = WxNotify(corpid=corpid, corpsecret=corpsecret, agentid=agentid,media_id=media_id)
+wn = WxNotify(corpid=QYWX_CORPID, corpsecret=QYWX_CORPSECRET, agentid=QYWX_AGENTID, media_id=QYWX_MEDIA_ID)
 wn.send('52破解签到信息', msg)
-
-
